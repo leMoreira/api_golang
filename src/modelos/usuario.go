94e482e0c0eb7f8538e838a7caa722d5bb009127
unknown <leandromoreira.dev@gmail.com>
2022-08-04 19:51:41 -0300

    Criando o model e o banco de dados

---
+++ src/modelos/usuario.go
@@ -0,0 +1,13 @@
+package modelos
+
+import "time"
+
+// usuario representa usuário utilizando redes social
+type Usuario struct {
+	ID       uint64    `json:"id,omitempty"`
+	Nome     string    `json:"nome,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
+	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
--- src/router/rotas/usuarios.go
+++ src/router/rotas/usuarios.go
@@ -25,13 +25,13 @@ var rotasUsuarios = []Rota{
 		RequerAutenticacao: false,
 	},
 	{
-		URI:                "/usuarios/{usuarioId",
+		URI:                "/usuarios/{usuarioId}",
 		Metodo:             http.MethodPut,
 		Funcao:             controllers.AtualizarUsuario,
 		RequerAutenticacao: false,
 	},
 	{
-		URI:                "/usuarios/{usuariosId",
+		URI:                "/usuarios/{usuariosId}",
 		Metodo:             http.MethodDelete,
 		Funcao:             controllers.DeletarUsuario,
 		RequerAutenticacao: false,
